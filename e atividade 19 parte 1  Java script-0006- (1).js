51a458f2b951d62857dfd35c87f13b3bcfcb7094
Bruno dos Passos <85364460+5223Bruno@users.noreply.github.com>
2023-06-27 21:48:30 -0400

    Add files via upload

---
+++ e atividade 19 parte 1  Java script-0006- (1).js
@@ -0,0 +1,10 @@
+var n1 = parseInt(prompt('Informe o primeiro valor:'));
+var n2 = parseInt(prompt('Infomre o segundo valor:'));
+
+
+
+result.innerHTML = n1+n2;
+
+var resp = confirm("Podemos continuar?");
+
+document.write("<p>" + resp + "<\p>");
